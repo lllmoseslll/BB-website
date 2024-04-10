287d1e5e9ad597f849e4a40e86b62b04c6457ea0
lllmoseslll <mosespeter620@gmail.com>
2024-04-10 17:30:45 +0300

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { store, persistor } from './redux/store.js'
+import {Provider} from "react-redux"
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <PersistGate persistor={persistor}>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
+  </PersistGate>
 )
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { useState } from "react";
+import {useDispatch, useSelector } from "react-redux";
+import {signInStart, signInSuccess, signInFailure} from "../redux/user/userSlice"
 
 
 
@@ -10,8 +12,8 @@ import { useState } from "react";
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState([null]);
-  const [loading, setLoading] = useState(false);
+  const {loading, error:errorMessage} = useSelector(state => state.user);
+  const dispatch =  useDispatch();
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -21,11 +23,11 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if(!formData.email || !formData.password){
-      return setErrorMessage('Please fill all fields');
+      // return setErrorMessage('Please fill all fields');
+      return dispatch(signInFailure("Please fill all fields"));
     }
     try {
-      setLoading(false);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
@@ -33,15 +35,18 @@ const SignIn = () => {
       });
       const data = await res.json();
       if(data.success === false){
-        return setErrorMessage(data.message);
+        // return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
+   
       if(res.ok){
+        dispatch(signInSuccess(data));
         navigate("/");
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false); 
+      // setErrorMessage(error.message);
+      // setLoading(false); 
+      dispatch(signInFailure(error.message));
     }
 
   };
